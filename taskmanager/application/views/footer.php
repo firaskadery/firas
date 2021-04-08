2f7f42b776f560bd491a923c2d303399f66f36cf
firaskadiri <firasalkadiri@gmail.com>
2021-04-08 10:32:15 +0300

    Update footer.php

--- taskmanager/application/views/footer.php
+++ taskmanager/application/views/footer.php
@@ -13,7 +13,7 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; <?php echo date('Y'); ?> <a href="https://adminlte.io">Firas.com</a>.</strong>
+    <strong>Copyright &copy; <?php echo date('Y'); ?> <a href="https://adminlte.io">Name.com</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version  </b><?php echo $this->config->item('app_version'); ?>
